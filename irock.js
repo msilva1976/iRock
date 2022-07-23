e64eadcfd54221936186070e67216167d65984fe
msilva1976 <marcosspom@gmail.com>
2022-07-23 18:43:01 -0300

    1 cominte

---
+++ irock.js
@@ -0,0 +1,12 @@
+
+//window.alert('Ola, eu sou a rocha de estimação');
+
+////function moveon(){
+    //exibe um caixa de dialogo modal para fazer uma pergunta para o usuario
+    var answser = confirm("pronto para proceguir?");
+    //se ele clicou n 'ok', faz o navegador carregar uma nova pagina
+    if(answser)window.location="http://google.com.br";
+
+//}  
+//execulta a função por 1 minuto a partir de agora
+setTimeout(moveon,60000);
